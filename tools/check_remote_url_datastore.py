f59a38d7b3544ce72e60bede6f956267530b566d
Timothy Chen <tnachen@gmail.com>
2021-04-23 12:57:48 -0700

    Use standard python libaries

--- tools/check_remote_url_datastore.py
+++ tools/check_remote_url_datastore.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
-import sys, os
+import sys
 import subprocess
+import os
+import requests
+import zipfile
+import filecmp
 
 if __name__ == '__main__':
     remote_url = sys.argv[1]
@@ -10,23 +14,25 @@ if __name__ == '__main__':
     target_dir = "expected_ds"
     os.mkdir(target_dir)
     target_file = os.path.join(target_dir, file_name)
+
+    r = requests.get(remote_url, allow_redirects=True)
+    
     # Download the remote file
-    subprocess.check_call(["curl", "-L", "-s", remote_url,
-                           "--output", target_file],
-                          stdout=sys.stdout,
-                          stderr=sys.stderr)
+    with open(target_file, 'wb') as f:
+        f.write(r.content)
+
     # Extract the file
-    subprocess.check_call(["unzip", target_file, "-d", target_dir],
-                          stdout=sys.stdout,
-                          stderr=sys.stderr)
+    with zipfile.ZipFile(target_file, 'r') as zip_ref:
+        zip_ref.extractall(target_dir)
 
     os.remove(target_file)
     
     # Check the datastore directory and downloaded datastore content
     # is the same
-    subprocess.check_call(["diff", "-r", target_dir, datastore_dir],
-                          stdout=sys.stdout,
-                          stderr=sys.stderr)
-
+    dircmp = filecmp.dircmp(target_dir, datastore_dir)
+    if len(dircmp.diff_files) > 0:
+        print(f"Datastore is different than expected, diffs: {dircmp.diff_files}")
+        sys.exit(1)
+    
     print("Datastore verified")
     
