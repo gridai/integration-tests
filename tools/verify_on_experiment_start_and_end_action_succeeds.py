61b7505525006f58c725c52d26d84baffe233f2e
Rick Izzo <rlizzo@users.noreply.github.com>
2022-02-11 01:35:14 -0500

    Update verify_on_experiment_start_and_end_action_succeeds.py

--- tools/verify_on_experiment_start_and_end_action_succeeds.py
+++ tools/verify_on_experiment_start_and_end_action_succeeds.py
@@ -7,9 +7,7 @@ def main(nfiles_out):
     start_file = Path("/file_created_by_on_experiment_start_action.txt")
     assert start_file.exists()
     
-    for i in range(nfiles_out):
-        with open(f"./file_{i}.out", "w+") as f:
-            f.write(f"this is the contents of file number: {i} ... {'foo \n' for _ in range(i)}")
+    print(nfiles_out)
     
     # this will be created by the on-experiment-end action. It shouldn't exist at runtime of the script.
     end_file = Path("/file_to_be_deleted_by_on_experiment_end_action.txt")
