b571288d4ec0c3df0bef00405f7b58b06b6f0feb
Rick Izzo <rlizzo@users.noreply.github.com>
2022-02-11 00:16:32 -0500

    Update verify_on_experiment_start_and_end_action_succeeds.py

--- tools/verify_on_experiment_start_and_end_action_succeeds.py
+++ tools/verify_on_experiment_start_and_end_action_succeeds.py
@@ -1,10 +1,16 @@
 from pathlib import Path
+from argparse import ArgumentParser
 
 
-def main():
+def main(nfiles_out):
     # this is created by the on-experiment-start action.
     start_file = Path("/file_created_by_on_experiment_start_action.txt")
     assert start_file.exists()
+    
+    for i in range(nfiles_out):
+        with open(f"./file_{i}.out", "w+") as f:
+            f.write(f"this is the contents of file number: {i} ... {'foo \n' for _ in range(i)}")
+    
     # this will be created by the on-experiment-end action. It shouldn't exist at runtime of the script.
     end_file = Path("/file_to_be_deleted_by_on_experiment_end_action.txt")
     assert not end_file.exists()
@@ -12,4 +18,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('--nfiles_out', default=0, type=int)
+    args = parser.parse_args()
+    main(args.nfiles_out)
