3816653f37aaf8c66d87308560678ffc60d350db
Rick Izzo <rlizzo@users.noreply.github.com>
2022-04-04 04:20:57 -0400

    Update check_file_exists.py

--- tools/check_file_exists.py
+++ tools/check_file_exists.py
@@ -4,6 +4,12 @@ import sys, os
 
 if __name__ == '__main__':
     print(f'sys.argv={sys.argv}')
-    with open(sys.argv[2], "r") as f:
-        for line in f.readlines():
-            print(line.rstrip(), flush=True)
+    try:
+        print(f"trying opening in read+text mode")
+        with open(sys.argv[2], "r") as f:
+            print("was able to open file successfully")
+    except UnicodeDecodeError as e:
+        print(f"catching UnicodeDecodeError after opening in text mode: {e}")
+        print(f"trying opening in read+binary mode")
+        with open(sys.argv[2], "rb") as f:
+            print("was able to open file successfully")
