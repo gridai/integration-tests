bee4feb6cceb0cd7bc8e9839da78ca858fce4217
Rick Izzo <rlizzo@users.noreply.github.com>
2022-04-04 03:50:42 -0400

    Update check_file_exists.py

--- tools/check_file_exists.py
+++ tools/check_file_exists.py
@@ -3,6 +3,7 @@
 import sys, os
 
 if __name__ == '__main__':
+    print(f'sys.argv={sys.argv}')
     with open(sys.argv[2], "r") as f:
         for line in f.readlines():
             print(line.rstrip(), flush=True)
