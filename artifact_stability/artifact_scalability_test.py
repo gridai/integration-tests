92a860f516b578681d614e511232a70bd1f6e387
Luca Furst <rlfurst@gmail.com>
2022-05-03 08:59:57 -0400

    GRID-9122: Add out_dir to script

--- artifact_stability/artifact_scalability_test.py
+++ artifact_stability/artifact_scalability_test.py
@@ -1,7 +1,8 @@
-# TODO: move to grid/integration-test repo
 from argparse import ArgumentParser
+import os
 
 parser = ArgumentParser()
+parser.add_argument("--out-dir", type=str)
 parser.add_argument("--num-exp", type=int, help="only purpose is to scale the hyperparameter sweep")
 parser.add_argument("--num-gb", type=int)
 parser.add_argument("--num-artifacts", type=int)
@@ -16,7 +17,7 @@ for i in range(args.num_iterations):
     print(f"writing batch {i} of {args.num_iterations}")
 
     for artifact_num in range(start, args.num_artifacts):
-        name = f"artifact-{artifact_num}.txt"
+        name = os.path.join(args.out_dir, f"artifact-{artifact_num}.txt")
         with open(name, 'w') as f:
             for gb in range(args.num_gb):
                 print(f"writing {gb}/{args.num_gb} for {name}")
