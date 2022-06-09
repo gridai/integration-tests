f2ad00e7cf0d4118c95d7d497100a48d7646baf2
Rick Izzo <rlizzo@users.noreply.github.com>
2022-06-09 15:00:58 -0400

    Update check_file_exists.py

--- tools/check_file_exists.py
+++ tools/check_file_exists.py
@@ -2,8 +2,19 @@
 
 import sys, os
 
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+
 if __name__ == '__main__':
     print(f'sys.argv={sys.argv}')
+    list_files(sys.argv[2])
     try:
         print(f"trying opening in read+text mode")
         with open(sys.argv[2], "r") as f:
